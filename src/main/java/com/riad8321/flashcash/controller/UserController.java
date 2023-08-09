bd40d33e25dd721d0d6665c633e4fcdd0ad0ca00
riadWf3 <riad.draoui@gmail.com>
2023-08-09 15:52:26 +0200

    Add cash features + add-cash view

--- src/main/java/com/riad8321/flashcash/controller/UserController.java
+++ src/main/java/com/riad8321/flashcash/controller/UserController.java
@@ -4,8 +4,13 @@ package com.riad8321.flashcash.controller;
 import com.riad8321.flashcash.model.User;
 import com.riad8321.flashcash.service.SessionService;
 import com.riad8321.flashcash.service.UserService;
+import com.riad8321.flashcash.service.form.AddCashForm;
 import com.riad8321.flashcash.service.form.AddIbanForm;
 import com.riad8321.flashcash.service.form.SignUpForm;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.tomcat.util.net.openssl.ciphers.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,7 +48,7 @@ public class UserController {
     }
 
 
-    @GetMapping("/index")
+    @GetMapping("/")
     public ModelAndView index(Model model) {
         User user = sessionService.sessionUser();
         model.addAttribute("user", user);
@@ -54,20 +59,37 @@ public class UserController {
     public ModelAndView showIbanRegisterForm(Model model) {
         User user = sessionService.sessionUser();
         model.addAttribute("user", user);
-        return new ModelAndView("add-iban");
+        return new ModelAndView("add-iban", "AddIbanForm", new  AddIbanForm());
     }
 
     @PostMapping(path="/add-iban")
-    public ModelAndView addIbanRegisterForm(@ModelAttribute("addIbanForm") AddIbanForm form) {
+    public ModelAndView addIbanRegisterForm(Model model, @ModelAttribute("addIbanForm") AddIbanForm form) {
         User user = sessionService.sessionUser();
-        System.out.println(form.toString());
         userService.ibanRegistration(form);
+        model.addAttribute("user, user");
         return new ModelAndView("add-iban");
     }
 
-    @GetMapping("/logout")
-    public String logOff(Model model) {
-        return "index";
+    @GetMapping("/add-cash")
+    public ModelAndView showAddCashForm(Model model) {
+        User user = sessionService.sessionUser();
+        model.addAttribute("user", user);
+        return new ModelAndView("add-cash", "AddIbanForm", new  AddIbanForm());
+    }
+
+    @PostMapping(path="/add-cash")
+    public ModelAndView showAddCashForm(Model model, @ModelAttribute("addCashForm") AddCashForm form) {
+        User user = sessionService.sessionUser();
+        userService.addCash(form);
+        model.addAttribute("user, user");
+        return new ModelAndView("add-cash");
+    }
+
+
+    @PostMapping("/logout")
+    public String performLogout() {
+
+        return "redirect:/";
     }
 }
 
